f75f2533089898cffc2b0f468c38b620415cd973
Adrian Zinko <adrian.zinko@boldare.com>
2022-06-19 20:17:38 +0200

    feat(): update next config for optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/products/',
+        destination: 'products/1',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: [
+      'naszsklep-api.vercel.app',
+      'media.graphcms.com',
+      'media.graphassets.com',
+    ],
+  },
+  formats: ['image/avif', 'image/webp'],
   reactStrictMode: true,
-}
+  trailingSlash: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
